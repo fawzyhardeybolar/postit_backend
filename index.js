68c30cb80ead1f2bef4800a4fab001cae27074eb
fawzyhardeybolar <fawazhardeybolar2002@gmail.com>
2023-04-28 23:03:48 +0100

    add code, add a new file

--- index.js
+++ index.js
@@ -1,2 +1,9 @@
 const express = require("express");
+const PORT = process.env
 const app = express();
+const mongoose = require("mongoose")
+const cors = require("cors")
+const connectDB = require("./connectDB")
+require("dotenv").config()
+
+connectDB()
