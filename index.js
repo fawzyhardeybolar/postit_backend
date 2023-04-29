de61ada3dcb65f43447cef4b2f18090a14cc1afb
fawzyhardeybolar <fawazhardeybolar2002@gmail.com>
2023-04-29 22:57:33 +0100

    connect to DB

--- index.js
+++ index.js
@@ -1,9 +1,22 @@
 const express = require("express");
-const PORT = process.env
+const PORT = process.env.PORT;
 const app = express();
-const mongoose = require("mongoose")
-const cors = require("cors")
-const connectDB = require("./connectDB")
-require("dotenv").config()
+const mongoose = require("mongoose");
+const cors = require("cors");
+const connectDB = require("./connectDB");
+require("dotenv").config();
 
-connectDB()
+// MIDDLEWARES
+app.use(express.json());
+app.use(cors());
+app.use(express.urlencoded({ extended: true }));
+
+const { MONGO_URI } = process.env;
+
+connectDB(MONGO_URI)
+  .then(() => {
+    app.listen(PORT, () => {
+      console.log("Server Connected...");
+    });
+  })
+  .catch((err) => console.log(err));
